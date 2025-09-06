CleverRaven/Cataclysm-DDA

Initial version of cache of map data at overmap scale

src/overmap.cpp
@@ -3154,6 +3154,11 @@ oter_id overmap::get_default_terrain( int z ) const
     return settings->default_oter[OVERMAP_DEPTH + z].id();
 }
 
+// underlying bitset default constructs to all 0.
+static std::shared_ptr<map_data_summary> impassable_omt( new map_data_summary() );
+static std::shared_ptr<map_data_summary> passable_omt( new map_data_summary(
+            ~impassable_omt->passable ) );
+
 void overmap::init_layers()
 {
     for( int k = 0; k < OVERMAP_LAYERS; ++k ) {
@@ -3162,6 +3167,8 @@ void overmap::init_layers()
         l.terrain.fill( tid );
         l.visible.fill( om_vision_level::unseen );
         l.explored.fill( false );
+        // Verify this isn't copying!
+        l.map_cache.fill( impassable_omt );
     }
 }
 
@@ -3202,6 +3209,12 @@ void overmap::ter_set( const tripoint_om_omt &p, const oter_id &id )
         }
         // We had a predecessor, and it was the same type as the incoming one
         // Don't push another copy.
+    }
+    // Jank to get something usable populated
+    if( id->get_travel_cost_type() == oter_travel_cost_type::impassable ) {
+        set_passable( project_combine( loc, p ), impassable_omt );
+    } else {
+        set_passable( project_combine( loc, p ), passable_omt );
     }
     current_oter = id;
 }
@@ -3586,6 +3599,69 @@ std::vector<point_abs_omt> overmap::find_extras( const int z, const std::string 
         }
     }
     return extra_locations;
+}
+
+bool overmap::passable( const tripoint_abs_ms &p )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_coord;
+    std::tie( overmap_coord, omt_coord ) = project_remain<coords::om>( project_to<coords::omt>( p ) );
+    if( overmap_coord != loc ) {
+        return false;
+    }
+    std::shared_ptr<map_data_summary> &ptr = layer[omt_coord.z() +
+            OVERMAP_DEPTH].map_cache[omt_coord.xy()];
+    if( !ptr ) {
+        // Oh no we aren't populated???
+        // Promote to error later.
+        return false;
+    }
+    point_abs_omt omt_origin;
+    tripoint_omt_ms index;
+    std::tie( omt_origin, index ) = project_remain<coords::omt>( p );
+    return ptr->passable[index.y() * 24 + index.x()];
+}
+
+// For internal use only, just overwrite the pointer.
+void overmap::set_passable( const tripoint_abs_omt &p,
+                            std::shared_ptr<map_data_summary> new_passable )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_coord;
+    std::tie( overmap_coord, omt_coord ) = project_remain<coords::om>( p );
+    if( overmap_coord != loc ) {
+        return;
+    }
+    std::shared_ptr<map_data_summary> &ptr = layer[omt_coord.z() +
+            OVERMAP_DEPTH].map_cache[omt_coord.xy()];
+    ptr = new_passable;
+}
+
+void overmap::set_passable( const tripoint_abs_omt &p, const std::bitset<24 * 24> &new_passable )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_coord;
+    std::tie( overmap_coord, omt_coord ) = project_remain<coords::om>( p );
+    if( overmap_coord != loc ) {
+        return;
+    }
+    std::shared_ptr<map_data_summary> &ptr = layer[omt_coord.z() +
+            OVERMAP_DEPTH].map_cache[omt_coord.xy()];
+    if( !ptr ) {
+        // Oh no we aren't populated???
+        // Promote to error later.
+        return;
+    }
+    ptr.reset( new map_data_summary( new_passable ) );
+}
+
+bool overmap::inbounds( const tripoint_abs_ms &p )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_within_overmap;
+    std::tie( overmap_coord, omt_within_overmap ) =
+        project_remain<coords::om>( project_to<coords::omt> ( p ) );
+    return overmap_coord == loc;
 }
 
 bool overmap::inbounds( const tripoint_om_omt &p, int clearance )

src/overmap.h
@@ -119,10 +119,21 @@ struct enum_traits<om_vision_level> {
     static constexpr om_vision_level last = om_vision_level::last;
 };
 
+// Summarises one OMT worth of map data for use by agents operating at overmap scale.
+// E.g. hordes, fire spread, NPCs.
+// Dimensions are 24x24
+struct map_data_summary {
+    map_data_summary() = default;
+    map_data_summary( std::bitset<24 * 24> new_passable ): passable( new_passable ) {}
+    // TODO: constant?
+    std::bitset<24 * 24> passable;
+};
+
 struct map_layer {
     cata::mdarray<oter_id, point_om_omt> terrain;
     cata::mdarray<om_vision_level, point_om_omt> visible;
     cata::mdarray<bool, point_om_omt> explored;
+    cata::mdarray<std::shared_ptr<map_data_summary>, point_om_omt> map_cache;
     std::vector<om_note> notes;
     std::vector<om_map_extra> extras;
 };
@@ -286,6 +297,14 @@ class overmap
         void delete_extra( const tripoint_om_omt &p );
 
         /**
+         * Access cache of map data for agents operating at overmap scale.
+         */
+        bool passable( const tripoint_abs_ms &p );
+        void set_passable( const tripoint_abs_omt &p, const std::bitset<24 * 24> &new_passable );
+    private:
+        void set_passable( const tripoint_abs_omt &p, std::shared_ptr<map_data_summary> new_passable );
+    public:
+        /**
          * Getter for overmap scents.
          * @returns a reference to a scent_trace from the requested location.
          */
@@ -303,6 +322,8 @@ class overmap
         static bool inbounds( const point_om_omt &p, int clearance = 0 ) {
             return inbounds( tripoint_om_omt( p, 0 ), clearance );
         }
+        bool inbounds( const tripoint_abs_ms &p );
+
         /**
          * Return a vector containing the absolute coordinates of
          * every matching note on the current z level of the current overmap.

OFCRohyon730/PoliceDDA

src/overmap.cpp
@@ -3192,6 +3192,11 @@ oter_id overmap::get_default_terrain( int z ) const
     return settings->default_oter[OVERMAP_DEPTH + z].id();
 }
 
+// underlying bitset default constructs to all 0.
+static std::shared_ptr<map_data_summary> impassable_omt( new map_data_summary() );
+static std::shared_ptr<map_data_summary> passable_omt( new map_data_summary(
+            ~impassable_omt->passable ) );
+
 void overmap::init_layers()
 {
     for( int k = 0; k < OVERMAP_LAYERS; ++k ) {
@@ -3200,6 +3205,8 @@ void overmap::init_layers()
         l.terrain.fill( tid );
         l.visible.fill( om_vision_level::unseen );
         l.explored.fill( false );
+        // Verify this isn't copying!
+        l.map_cache.fill( passable_omt );
     }
 }
 
@@ -3240,6 +3247,12 @@ void overmap::ter_set( const tripoint_om_omt &p, const oter_id &id )
         }
         // We had a predecessor, and it was the same type as the incoming one
         // Don't push another copy.
+    }
+    // Jank to get something usable populated
+    if( id->get_travel_cost_type() == oter_travel_cost_type::impassable ) {
+        set_passable( project_combine( loc, p ), impassable_omt );
+    } else {
+        set_passable( project_combine( loc, p ), passable_omt );
     }
     current_oter = id;
 }
@@ -3654,6 +3667,63 @@ void overmap::add_omt_stack_argument( const point_abs_omt &p, const std::string 
                                       const cata_variant &value )
 {
     omt_stack_arguments_map[p].add( param_name, value );
+}
+
+bool overmap::passable( const tripoint_om_ms &p )
+{
+    point_om_omt omt_origin;
+    tripoint_omt_ms index;
+    std::tie( omt_origin, index ) = project_remain<coords::omt>( p );
+    std::shared_ptr<map_data_summary> &ptr = layer[index.z() +
+            OVERMAP_DEPTH].map_cache[omt_origin];
+    if( !ptr ) {
+        // Oh no we aren't populated???
+        // Promote to error later.
+        return false;
+    }
+    return ptr->passable[index.y() * 24 + index.x()];
+}
+
+// For internal use only, just overwrite the pointer.
+void overmap::set_passable( const tripoint_abs_omt &p,
+                            std::shared_ptr<map_data_summary> new_passable )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_coord;
+    std::tie( overmap_coord, omt_coord ) = project_remain<coords::om>( p );
+    if( overmap_coord != loc ) {
+        return;
+    }
+    std::shared_ptr<map_data_summary> &ptr = layer[omt_coord.z() +
+            OVERMAP_DEPTH].map_cache[omt_coord.xy()];
+    ptr = new_passable;
+}
+
+void overmap::set_passable( const tripoint_abs_omt &p, const std::bitset<24 * 24> &new_passable )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_coord;
+    std::tie( overmap_coord, omt_coord ) = project_remain<coords::om>( p );
+    if( overmap_coord != loc ) {
+        return;
+    }
+    std::shared_ptr<map_data_summary> &ptr = layer[omt_coord.z() +
+            OVERMAP_DEPTH].map_cache[omt_coord.xy()];
+    if( !ptr ) {
+        // Oh no we aren't populated???
+        // Promote to error later.
+        return;
+    }
+    ptr.reset( new map_data_summary( new_passable ) );
+}
+
+bool overmap::inbounds( const tripoint_abs_ms &p )
+{
+    point_abs_om overmap_coord;
+    tripoint_om_omt omt_within_overmap;
+    std::tie( overmap_coord, omt_within_overmap ) =
+        project_remain<coords::om>( project_to<coords::omt> ( p ) );
+    return overmap_coord == loc;
 }
 
 bool overmap::inbounds( const tripoint_om_omt &p, int clearance )

src/overmap.h
@@ -124,10 +124,21 @@ struct enum_traits<om_vision_level> {
     static constexpr om_vision_level last = om_vision_level::last;
 };
 
+// Summarises one OMT worth of map data for use by agents operating at overmap scale.
+// E.g. hordes, fire spread, NPCs.
+// Dimensions are 24x24
+struct map_data_summary {
+    map_data_summary() = default;
+    map_data_summary( std::bitset<24 * 24> new_passable ): passable( new_passable ) {}
+    // TODO: constant?
+    std::bitset<24 * 24> passable;
+};
+
 struct map_layer {
     cata::mdarray<oter_id, point_om_omt> terrain;
     cata::mdarray<om_vision_level, point_om_omt> visible;
     cata::mdarray<bool, point_om_omt> explored;
+    cata::mdarray<std::shared_ptr<map_data_summary>, point_om_omt> map_cache;
     std::vector<om_note> notes;
     std::vector<om_map_extra> extras;
 };
@@ -374,6 +385,14 @@ class overmap
         void delete_extra( const tripoint_om_omt &p );
 
         /**
+         * Access cache of map data for agents operating at overmap scale.
+         */
+        bool passable( const tripoint_om_ms &p );
+        void set_passable( const tripoint_abs_omt &p, const std::bitset<24 * 24> &new_passable );
+    private:
+        void set_passable( const tripoint_abs_omt &p, std::shared_ptr<map_data_summary> new_passable );
+    public:
+        /**
          * Getter for overmap scents.
          * @returns a reference to a scent_trace from the requested location.
          */
@@ -391,6 +410,8 @@ class overmap
         static bool inbounds( const point_om_omt &p, int clearance = 0 ) {
             return inbounds( tripoint_om_omt( p, 0 ), clearance );
         }
+        bool inbounds( const tripoint_abs_ms &p );
+
         /**
          * Return a vector containing the absolute coordinates of
          * every matching note on the current z level of the current overmap.